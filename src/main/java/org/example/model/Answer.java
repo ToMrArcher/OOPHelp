84ed6c69324703be45d7aada7bae9bee86842331
tomrarcher <syvertcollege@gmail.com>
2023-06-12 21:07:56 +0200

    Added Question and Answer models

--- src/main/java/org/example/model/Answer.java
+++ src/main/java/org/example/model/Answer.java
@@ -3,5 +3,26 @@ package org.example.model;
 public class Answer {
 
     private String answerText;
+    private boolean isCorrect;
 
+    public Answer(String answerText, boolean isCorrect) {
+        this.answerText = answerText;
+        this.isCorrect = isCorrect;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
 }
--- src/main/java/org/example/model/Question.java
+++ src/main/java/org/example/model/Question.java
@@ -1,12 +1,34 @@
 package org.example.model;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Question {
 
     private String questionText;
     private ArrayList<Answer> answers;
 
+    public void printQuestion(){
+        System.out.println(questionText);
+        for(int i = 1; i <= answers.size(); i++){
+            System.out.println(i + ". " + answers.get(i-1));
+        }
+    }
 
+    public boolean getAnswerFromUser(){
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String inputFromUser = scanner.nextLine();
+            int pickedAnswer = Integer.parseInt(inputFromUser);
+            if(pickedAnswer < 0 || pickedAnswer > answers.size()){
+                System.out.println("Please pick a valid answer");
+                continue;
+            }
+            if(answers.get(pickedAnswer).isCorrect()){
+                return true;
+            }
+            return false;
+        }
+    }
 
 }
